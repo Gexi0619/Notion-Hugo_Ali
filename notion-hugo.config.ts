66b1b2b0b96e42f8d16e466fcc00cebf3e811262
Gexi0619 <sungexi@hotmail.com>
2024-08-26 00:49:00 +0200

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-63691d34eb8841f0a277d60126cf7416?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
